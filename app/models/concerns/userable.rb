dbd50348de95c51a4b9d242e9afe07ca2616afaa
Ejay Canaria <ejaypcanaria@gmail.com>
2014-11-09 20:16:13 +0800

    Refactor duplicate user related codes into concern

---
+++ app/models/concerns/userable.rb
@@ -0,0 +1,10 @@
+module Userable
+  extend ActiveSupport::Concern
+
+  included do
+    has_one :user, as: :meta, dependent: :destroy
+
+    accepts_nested_attributes_for :user
+    delegate :first_name, :last_name, :full_name, :email, to: :user
+  end
+end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,9 +1,6 @@
 class Teacher < ActiveRecord::Base
-  has_one :user, as: :meta, dependent: :destroy
-  accepts_nested_attributes_for :user
-  
+  include Userable
+
   has_many :subjects
   has_many :periods, through: :subjects
-
-  delegate :first_name, :last_name, :full_name, :email, to: :user
 end
